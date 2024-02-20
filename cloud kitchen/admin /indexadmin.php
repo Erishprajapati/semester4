82ecdb5fedb7d58cbc951eb72389324852498321
Erishprajapati <irishmjn@gmail.com>
2024-02-20 22:17:16 +0545

    admin panel

---
+++ cloud kitchen/admin /indexadmin.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+     <title>Cloud kitchen - Homepage</title>
+     <link rel="stylesheet" href="../styles/admin.css">
+    </head>
+    <body>
+        <!-- Menu section start-->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+           <li><a href="#">Home</a></li>
+           <li><a href="#">Admin</a></li>
+           <li><a href="#">Category</a></li>
+           <li><a href="#">Food</a></li>
+           <li><a href="#">Order</a></li>
+           </ul>
+            </div>
+        </div>
+        <!--menu section ends-->
+
+        <!-- main content section start-->
+        <div class="main-content">
+            <div class=wrapper>
+             Main content goes here
+            </div>
+        </div>
+        <!-- Main content section ends-->
+
+        <!-- footer section starts-->
+        <div class="footer">
+            <div class="wrapper">
+             <p>2024 All rights reserved</p>
+             </div>
+            </div>
+        <!--footer section ends-->
+    </body>
+</html>
