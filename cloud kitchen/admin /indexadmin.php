f22fdf142a263cf06c41a2bf9d8e91cbc1aefce0
Erishprajapati <irishmjn@gmail.com>
2024-03-04 11:39:10 +0545

    worked on food/manage

--- cloud kitchen/admin /indexadmin.php
+++ cloud kitchen/admin /indexadmin.php
@@ -1,37 +1,95 @@
-<html>
-    <head>
-     <title>Cloud kitchen - Homepage</title>
-     <link rel="stylesheet" href="../styles/admin.css">
-    </head>
-    <body>
-        <!-- Menu section start-->
-        <div class="menu text-center">
-            <div class="wrapper">
-                <ul>
-           <li><a href="#">Home</a></li>
-           <li><a href="#">Admin</a></li>
-           <li><a href="#">Category</a></li>
-           <li><a href="#">Food</a></li>
-           <li><a href="#">Order</a></li>
-           </ul>
-            </div>
-        </div>
-        <!--menu section ends-->
 
-        <!-- main content section start-->
+<?php include('../fronts/menu.php'); ?>
+
+        <!-- Main Content Section Starts -->
         <div class="main-content">
-            <div class=wrapper>
-             Main content goes here
+            <div class="wrapper">
+                <h1>Dashboard</h1>
+                <br><br>
+                <?php 
+                    if(isset($_SESSION['login']))
+                    {
+                        echo $_SESSION['login'];
+                        unset($_SESSION['login']);
+                    }
+                ?>
+                <br><br>
+
+                <div class="col-4 text-center">
+
+                    <?php 
+                        //Sql Query 
+                        $sql = "SELECT * FROM tbl_category";
+                        //Execute Query
+                        $res = mysqli_query($conn, $sql);
+                        //Count Rows
+                        $count = mysqli_num_rows($res);
+                    ?>
+
+                    <h1><?php echo $count; ?></h1>
+                    <br />
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+
+                    <?php 
+                        //Sql Query 
+                        $sql2 = "SELECT * FROM tbl_food";
+                        //Execute Query
+                        $res2 = mysqli_query($conn, $sql2);
+                        //Count Rows
+                        $count2 = mysqli_num_rows($res2);
+                    ?>
+
+                    <h1><?php echo $count2; ?></h1>
+                    <br />
+                    Foods
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                    <?php 
+                        //Sql Query 
+                        $sql3 = "SELECT * FROM tbl_order";
+                        //Execute Query
+                        $res3 = mysqli_query($conn, $sql3);
+                        //Count Rows
+                        $count3 = mysqli_num_rows($res3);
+                    ?>
+
+                    <h1><?php echo $count3; ?></h1>
+                    <br />
+                    Total Orders
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                    <?php 
+                        //Creat SQL Query to Get Total Revenue Generated
+                        //Aggregate Function in SQL
+                        $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Delivered'";
+
+                        //Execute the Query
+                        $res4 = mysqli_query($conn, $sql4);
+
+                        //Get the VAlue
+                        $row4 = mysqli_fetch_assoc($res4);
+                        
+                        //GEt the Total REvenue
+                        $total_revenue = $row4['Total'];
+
+                    ?>
+
+                    <h1>$<?php echo $total_revenue; ?></h1>
+                    <br />
+                    Revenue Generated
+                </div>
+
+                <div class="clearfix"></div>
+
             </div>
         </div>
-        <!-- Main content section ends-->
+        <!-- Main Content Setion Ends -->
 
-        <!-- footer section starts-->
-        <div class="footer">
-            <div class="wrapper">
-             <p>2024 All rights reserved</p>
-             </div>
-            </div>
-        <!--footer section ends-->
-    </body>
-</html>
+<?php include('partials/footer.php') ?>
